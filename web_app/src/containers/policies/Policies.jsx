837d95d3768c75aa8830c6616798341b3cd6a527
jesse <itsjessef@gmail.com>
2024-04-05 14:19:59 -0700

    Update policy links

--- web_app/src/containers/policies/Policies.jsx
+++ web_app/src/containers/policies/Policies.jsx
@@ -11,11 +11,11 @@ const Policies = () => (
       <div className="morphic__event_description">
         <ul>
           <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/Morphic%20Code%20of%20Conduct%20for%20Events.pdf" target="_blank" rel="noopener noreferrer">Code of Conduct</a></li>
-          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/MorPhiC%20-%20Publication%20Policy.pdf" target="_blank" rel="noopener noreferrer">Publication policy</a></li>
-          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/Morphic%20-%20Software%20Sharing%20Policy.pdf" target="_blank" rel="noopener noreferrer">Software sharing policy</a></li>
-          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/Morphic%20-%20Human%20Genome%20Assembly%20Policy.pdf" target="_blank" rel="noopener noreferrer">Human Genome Assembly policy</a></li>
-          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/MorPhiC%20Affiliate%20Membership.pdf" target="_blank" rel="noopener noreferrer">Affiliate membership policy</a></li>
-          <li>Data sharing policy (pending)</li>
+          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/MorPhiC%20-%20Publication%20Policy.pdf" target="_blank" rel="noopener noreferrer">Publication Policy</a></li>
+          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/Morphic%20-%20Software%20Sharing%20Policy.pdf" target="_blank" rel="noopener noreferrer">Software Sharing Policy</a></li>
+          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/Morphic%20-%20Human%20Genome%20Assembly%20Policy.pdf" target="_blank" rel="noopener noreferrer">Human Genome Assembly Policy</a></li>
+          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/MorPhiC%20Affiliate%20Membership.pdf" target="_blank" rel="noopener noreferrer">Affiliate Membership Policy</a></li>
+          <li><a href="https://github.com/morphic-bio/morphic-bio.github.io/blob/main/Policies/Morphic%20-%20Data%20Release%20and%20Use%20Policy.pdf" target="_blank" rel="noopener noreferrer">Data Release and Use Policy</a></li>
         </ul>
       </div>
     </article>

02dcdc63fd957fd742c8b489233db8144653d4b1
jesse <itsjessef@gmail.com>
2024-04-30 12:44:24 -0700

    Remove unused files

--- web_app/src/containers/index.js
+++ web_app/src/containers/index.js
@@ -1,8 +1,6 @@
 export { default as Blog } from './blog/Blog';
-export { default as Features } from './features/Features';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
-export { default as Possibility } from './possibility/Possibility';
 export { default as Whatmorphic } from './whatmorphic/Whatmorphic';
 export { default as Map } from './map/Map';
 
